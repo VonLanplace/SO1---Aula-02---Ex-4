921353a09ff9118314d0e924a51e6cb18a9f1330
VonLanplace <firekstar@gmail.com>
2025-08-25 01:54:45 -0300

    Adicionado o metodo de executar processo

--- src/controller/RunController.java
+++ src/controller/RunController.java
@@ -7,21 +7,43 @@ import javax.swing.JFrame;
 import javax.swing.JTextField;
 
 public class RunController {
+	JTextField textPath;
 
 	public RunController(JTextField textPath, JFrame frame) {
+		this.textPath = textPath;
 		// TODO Auto-generated constructor stub
 	}
 
 	private void runPerform() {
-		System.out.println(false);
-		// TODO
+		try {
+			String[] command = textPath.getText().split(" ");
+			Process process = Runtime.getRuntime().exec(command);
+
+		} catch (Exception e) {
+			String msg = e.getMessage();
+
+			if (msg.contains("740")) {
+				StringBuffer buffer = new StringBuffer();
+				buffer.append("cmd /c");
+				buffer.append(" ");
+				buffer.append(textPath.getText());
+				runPerform();
+
+			} else {
+				System.err.println(msg);
+			}
+		}
 	}
 
 	public ActionListener getListener() {
 		return new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				runPerform();
+				try {
+					runPerform();
+				} catch (Exception er) {
+					// TODO: handle exception
+				}
 			}
 		};
 	}
